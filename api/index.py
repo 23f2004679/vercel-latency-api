ae27fb82e9a8771f34ab4e7ccc61f1cb5bb83a9d
Susmita Bhattacherjee <23f2004679@ds.study.iitm.ac.in>
2026-09-23 22:10:51 +0530

    Allow CORS from all origins

--- api/index.py
+++ api/index.py
@@ -12,20 +12,11 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_methods=["POST", "OPTIONS"],
-    allow_headers=["*"],
     allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
-@app.middleware("http")
-async def add_cors_headers(request, call_next):
-    response = await call_next(request)
-
-    response.headers["Access-Control-Allow-Origin"] = "*"
-    response.headers["Access-Control-Allow-Methods"] = "POST, OPTIONS"
-    response.headers["Access-Control-Allow-Headers"] = "*"
-
-    return response
 
 # Load the telemetry JSON file
 data_file = os.path.join(
