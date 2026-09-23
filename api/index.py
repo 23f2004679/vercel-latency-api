5a294270c8dad1d0f401054d8c824d7a415a0067
Susmita Bhattacherjee <23f2004679@ds.study.iitm.ac.in>
2026-09-23 22:02:42 +0530

    Ensure CORS headers on all responses

--- api/index.py
+++ api/index.py
@@ -14,7 +14,18 @@ app.add_middleware(
     allow_origins=["*"],
     allow_methods=["POST", "OPTIONS"],
     allow_headers=["*"],
+    allow_credentials=False,
 )
+
+@app.middleware("http")
+async def add_cors_headers(request, call_next):
+    response = await call_next(request)
+
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "POST, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "*"
+
+    return response
 
 # Load the telemetry JSON file
 data_file = os.path.join(
